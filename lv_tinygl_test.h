b7e3a4e99aab130e538f9dad52ff6029f877ad91
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2020-03-20 14:51:43 +0100

    add test

---
+++ lv_tinygl_test.h
@@ -0,0 +1,37 @@
+/**
+ * @file lv_templ.h
+ *
+ */
+
+#ifndef LV_TINYGL_TEST_H
+#define LV_TINYGL_TEST_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+/*********************
+ *      DEFINES
+ *********************/
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_TINYGL_TEST_H*/
